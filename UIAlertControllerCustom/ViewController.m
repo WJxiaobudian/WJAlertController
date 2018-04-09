da0fa39edf6f9f5062ad837acb0e582f58e4caf9
WJxiaobudian <490880226@qq.com>
2018-04-09 17:15:20 +0800

    测试

--- UIAlertControllerCustom/ViewController.m
+++ UIAlertControllerCustom/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "AlertControllerCustom.h"
 @interface ViewController ()
 
 @end
@@ -17,8 +17,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
 }
 
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    
+    // 中间弹出
+//    [[AlertControllerCustom shareInstance] showType:UIAlertControllerStyleAlert title:@"中间弹出" message:@"中间弹出" cancelTitle:@"取消" titleArray:@[@"123", @"234"] viewController:self view:nil confirm:^(NSInteger buttonTag) {
+//
+//        if (buttonTag == -1) {
+//            NSLog(@"取消");
+//        }
+//        else {
+//            NSLog(@"123");
+//        }
+//    }];
+    
+    [[AlertControllerCustom shareInstance] showType:UIAlertControllerStyleActionSheet title:@"" message:nil cancelTitle:@"取消" titleArray:@[@"123", @"234", @"2342", @"23242", @"1213"] viewController:self view:nil confirm:^(NSInteger buttonTag) {
+        
+        if (buttonTag == -1) {
+            NSLog(@"取消");
+        }
+        else {
+            NSLog(@"123");
+        }
+    }];
+
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
